4e6050d205854b746a83fa211397f41982ca6f2a
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-06-10 10:39:37 +0530

    ..

--- slmsproject/account/urls.py
+++ slmsproject/account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home,register,loginview,dashboard,logout_view
 urlpatterns=[
     path('home/',home,name='home'),
+    path('register/',register,name='register'),
+    path('login/',loginview,name='login'),
+    path('admin_dash/',dashboard,name='admin_dash'),
+    path('logout/',logout_view,name='logout')
+
 ]
--- slmsproject/account/views.py
+++ slmsproject/account/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from .models import Profile
+from django.contrib.auth  import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 def home(request):
     return render(request,'home.html')
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        role = request.POST['role']
+        user = User.objects.create_user(username=username, password=password)
+        Profile.objects.create(user=user, role=role)
+        return redirect('login')
+    return render(request, 'register.html')
+def loginview(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            if user.profile.role == 'admin':
+                return redirect('admin_dash')
+            return HttpResponse("staff")
+        return HttpResponse("Username and password invalid")
+    return render(request, 'login.html')
+@login_required
+def dashboard(request):
+    if request.user.profile.role!='admin':
+        return HttpResponse("staff_dashboard")
+    return render(request,'admin_dashboard.html')
+def logout_view(request):
+    logout(request)
+    return redirect('home')
